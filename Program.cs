97ea3a31b23bf60e21964453ea2c6ae911fa11e8
loganator956 <werktxgnpyerpvgmuokc@gmail.com>
2022-03-14 11:31:14 +0000

    created new console .net5.0 project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMP1903M_Assessment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
