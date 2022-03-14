c67ff40aba99b6297b215d03e01edbaa3ff9ead7
loganator956 <werktxgnpyerpvgmuokc@gmail.com>
2022-03-14 11:32:59 +0000

    imported base code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CMP1903M_Assessment_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
+            //Local list of integers to hold the first five measurements of the text
+            List<int> parameters = new List<int>();
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+
+
+            //Create an 'Analyse' object
+            //Pass the text input to the 'analyseText' method
+
+
+            //Receive a list of integers back
+
+
+            //Report the results of the analysis
+
+
+            //TO ADD: Get the frequency of individual letters?
+
+           
         }
+        
+        
+    
     }
 }
---
+++ Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1
+{
+    class Report
+    {
+        //Handles the reporting of the analysis
+        //Maybe have different methods for different formats of output?
+        //eg.   public void outputConsole(List<int>)
+
+    }
+}
