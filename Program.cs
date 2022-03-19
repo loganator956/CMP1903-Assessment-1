98761b0d3066ef03c644bd43af8f447598d2457c
loganator956 <werktxgnpyerpvgmuokc@gmail.com>
2022-03-19 20:55:22 +0000

    removed template comments

--- Program.cs
+++ Program.cs
@@ -11,27 +11,7 @@ namespace CMP1903M_Assessment_1
     {
         static void Main()
         {
-            //Local list of integers to hold the first five measurements of the text
-            List<int> parameters = new List<int>();
-
-            //Create 'Input' object
-            //Get either manually entered text, or text from a file
-            Console.WriteLine(Input.ManualTextInput());
-
-
-            //Create an 'Analyse' object
-            //Pass the text input to the 'analyseText' method
-
-
-            //Receive a list of integers back
-
-
-            //Report the results of the analysis
-
-
-            //TO ADD: Get the frequency of individual letters?
-
-           
+            
         }
     }
 }
