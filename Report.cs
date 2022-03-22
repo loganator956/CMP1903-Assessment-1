17a31a5055e397056ac3c121db5f80c3a62af7f0
loganator956 <werktxgnpyerpvgmuokc@gmail.com>
2022-03-22 10:54:16 +0000

    Changed format of output

--- Report.cs
+++ Report.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CMP1903M_Assessment_1.Debugging;
 
 namespace CMP1903M_Assessment_1
 {
@@ -19,11 +21,11 @@ namespace CMP1903M_Assessment_1
         private static string GenerateReportText(Analyse analysis)
         {
             return (@$"Analysis of Input:
-            Number of sentences: {analysis.Statistics[0]}
-            Number of vowels: {analysis.Statistics[1]}
-            Number of consonants: {analysis.Statistics[2]}
-            Number of upper case letters: {analysis.Statistics[3]}
-            Number of lower case letters: {analysis.Statistics[4]}");
+    Number of sentences:            {analysis.Statistics[0]}
+    Number of vowels:               {analysis.Statistics[1]}
+    Number of consonants:           {analysis.Statistics[2]}
+    Number of upper case letters:   {analysis.Statistics[3]}
+    Number of lower case letters:   {analysis.Statistics[4]}");
         }
 
         private const string DefaultReportFilePath = "report.txt";
@@ -35,7 +37,17 @@ namespace CMP1903M_Assessment_1
 
         public static void ReportToFile(Analyse analysis, string outputPath)
         {
-
+            Console.Write("\nSaving report to file");
+            try
+            {
+                File.WriteAllText(outputPath, GenerateReportText(analysis));
+            Console.Write($"\rSuccessfully saved report to the file {outputPath}\n");
+            }
+            catch (Exception except)
+            {
+                Debug.LogError(except.Message);
+                Debug.LogError("Saving the file had errors :(. Exception message above.");
+            }
         }
     }
 }
